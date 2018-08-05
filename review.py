b3f6deb954e81d448e48169f1e3b909119acd237
xuwei <xuwei@xu-wei.cn>
2018-08-06 00:14:49 +0800

    20180806 增加输入防错误代码

--- review.py
+++ review.py
@@ -15,10 +15,11 @@ cursor = conn.cursor()
 cursor.execute("select * from word where unit = %s and wkey = %s",(unit,"2"))
 # cursor.execute("select * from word where unit = 1;")
 values = cursor.fetchall()
-print(values)
+#print(values)
 total = len(values)
+print("本次测试单词量："+str(total))
 
-#测试模块
+print('...........................................')
 random.shuffle(values)
 # print(values)
 i=1
@@ -26,22 +27,25 @@ for word in values:
     print(str(i)+'：'+word[1])
     input("回车查看答案")
     print(str(word[0])+'：'+word[2])
-    key = input("认识请输入“1”，不认识输入“2”,退出输入“3:”：")
-    print(" ")
-    # if key ==3:
-    #     os._exit(0)
-    #     print("exit")
-    i=i+1
-    cursor.execute("UPDATE word SET wkey= %s  WHERE eng= %s",(key,word[1]))
-    conn.commit()
+    while True:
+        key = input("认识请输入“1”，不认识输入“2”,退出输入“3:”：")
+        print(key)
+        print(" ")
+        if key=='1' or key=='2' :
+            break
+            cursor.execute("UPDATE word SET wkey= %s  WHERE eng= %s", (key, word[1]))
+            conn.commit()
+            i=i+1
+            # break
 
 #数据库结果读取
 cursor.execute("select * from word where unit = %s and wkey = %s",(unit,"2"))
 values_end = cursor.fetchall()
-print(values_end)
+#print(values_end)
 values_end.sort(key=operator.itemgetter(0))
 errorlist = len(values_end)
 i=1
+print("不认识的单词：........................................")
 for word in values_end:
     print(str(i)+'：'+str(word[0])+'：'+word[1]+'：'+word[2])
     i=i+1
